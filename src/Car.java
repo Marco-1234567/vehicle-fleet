468a54ebdf2a7764d20445cc79986c2809d223a3
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-09-24 13:54:47 +0200

    Make car class

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car
+//extends Vehicle implements Maintainable
+{
+    String type;
+//    public  Car (int velocity, int weight, String fuel, String type){
+//        super(velocity, weight, fuel);
+//        this.type=type
+//    }
+
+}
