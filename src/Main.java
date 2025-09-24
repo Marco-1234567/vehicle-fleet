d6dc502d5734343f63f200e115365d7d43a50953
Sara Gyllentid <sara.gyllentid@gmail.com>
2025-09-24 14:27:21 +0200

    Flyttat bus, car och truck till vehicles package. Uppdaterat mainfilen och filerna har fått varsin package

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import vehicles.Bus;
+import vehicles.Car;
+import vehicles.Truck;
+
 public class Main {
     static void main() {
 
---
+++ src/vehicles/Truck.java
@@ -0,0 +1,8 @@
+package vehicles;
+public class Truck extends Vehicle{
+    public Truck(int velocity, int weight, String fuel){
+        super(velocity, weight, fuel);
+
+
+    }
+}
