1dd36ebe8e7b191f516d8a2e6085213bb81ca34e
Marco <laasonen.marco@yahoo.com>
2025-09-24 15:37:33 +0200

    fixes

--- src/vehicles/Bus.java
+++ src/vehicles/Bus.java
@@ -4,12 +4,14 @@ import baseclasses.Vehicle;
 
 public class Bus extends Vehicle {
 
+    @Override
+    public void move(Vehicle vehicle) {
+
+    }
+
     public Bus(int velocity, int weight, String fuel){
         super(velocity, weight, fuel);
     }
-
-    @Override
-    public void move() {
 
     }
 }
