5589a723fae316d03e82e8e8dde33092e21ad191
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-09-25 10:36:02 +0200

    add type to class

--- src/vehicles/Car.java
+++ src/vehicles/Car.java
@@ -2,13 +2,24 @@ package vehicles;
 import baseclasses.Vehicle;
 
 public class Car extends Vehicle {
+    private String brand;
+    private String model;
 
     public  Car (int velocity, int weight, String fuel){
         super( velocity, weight, fuel);
+        this.brand = brand;
+        this.model = model;
     }
 
     @Override
     public void move() {
+    }
 
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
