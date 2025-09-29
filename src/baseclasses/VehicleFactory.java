138b4cd04e5948a1c4fc63cc4f86c840c9b8a5fb
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-09-29 17:05:57 +0200

    Implement different vehicle creator based on type of vehicle

--- src/baseclasses/VehicleFactory.java
+++ src/baseclasses/VehicleFactory.java
@@ -5,12 +5,42 @@ import vehicles.Car;
 import vehicles.Truck;
 
 public class VehicleFactory {
-    public static Vehicle createVehicle(String type, String name){
-        return  switch (type.toLowerCase()){
-            case "car" -> new Car.Builder().name(name).velocity(120).weight(1000).fuel("Gasoline").milageSinceLastService(0).brand("Toyota").model("xs32").build();
-            case "bus" -> new Bus.Builder().name(name).velocity(80).weight(5000).fuel("Diesel").milageSinceLastService(0).seats(40).build();
-            case "truck" -> new Truck.Builder().name(name).velocity(70).weight(8000).fuel("Diesel").milageSinceLastService(0).loadCapacity(10000).currentLoad(0).build();
-            default -> throw new IllegalStateException("Unexpected value: " + type.toLowerCase());
-        };
+
+    public static Car createCar(String name, int velocity, int weight, String fuel, double milageSinceLastService, String brand, String model) {
+        return (Car) new Car.Builder()
+                .name(name)
+                .velocity(velocity)
+                .weight(weight)
+                .fuel(fuel)
+                .milageSinceLastService(milageSinceLastService)
+                .brand(brand)
+                .model(model)
+                .build();
     }
+
+
+    public static Bus createBus(String name, int velocity, int weight, String fuel, double milageSinceLastService, int seats) {
+        return (Bus) new Bus.Builder()
+                .name(name)
+                .velocity(velocity)
+                .weight(weight)
+                .fuel(fuel)
+                .milageSinceLastService(milageSinceLastService)
+                .seats(seats)
+                .build();
+    }
+
+    public static Truck createTruck(String name, int velocity, int weight, String fuel, double milageSinceLastService, int loadCapacity, int currentLoad) {
+        return (Truck) new Truck.Builder()
+                .name(name)
+                .velocity(velocity)
+                .weight(weight)
+                .fuel(fuel)
+                .milageSinceLastService(milageSinceLastService)
+                .loadCapacity(loadCapacity)
+                .currentLoad(currentLoad)
+                .build();
+    }
+
+
 }
