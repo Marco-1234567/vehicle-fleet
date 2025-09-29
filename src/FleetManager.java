498f5fd29860158ee9a5d7f29f93142a43d5690a
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-09-29 15:36:11 +0200

    Add vehicleFactory

--- src/FleetManager.java
+++ src/FleetManager.java
@@ -1,4 +1,5 @@
 import baseclasses.Vehicle;
+import baseclasses.VehicleFactory;
 import vehicles.Bus;
 import vehicles.Car;
 import vehicles.Maintainable;
@@ -23,10 +24,13 @@ public class FleetManager {
     public void run(){
 
         List<Vehicle> vehicleList = new ArrayList<>( List.of(
-                new Bus.Builder().velocity(100).weight(2000).fuel("Diesel").milage(1500).milageSinceLastService(1700).name("bus  1").seats(54).build(),
-                new Truck.Builder().velocity(80).weight(5000).fuel("Diesel").milage(1500).milageSinceLastService(1245).name("truck  1").loadCapacity(100).currentLoad(20).build(),
-                new Truck.Builder().velocity(80).weight(5000).fuel("Diesel").milage(1500).milageSinceLastService(1245).name("truck  2").loadCapacity(5000).currentLoad(3000).build(),
-                new Car.Builder().velocity(80).weight(5000).fuel("Diesel").milage(1500).milageSinceLastService(1245).name("car  1").brand("Fiat").model("x25").build()
+                VehicleFactory.createVehicle("car", "car 1"),
+                VehicleFactory.createVehicle("bus", "bus 1"),
+                VehicleFactory.createVehicle("truck", "truck 1")
+//                new Bus.Builder().velocity(100).weight(2000).fuel("Diesel").milage(1500).milageSinceLastService(1700).name("bus  1").seats(54).build(),
+//                new Truck.Builder().velocity(80).weight(5000).fuel("Diesel").milage(1500).milageSinceLastService(1245).name("truck  1").loadCapacity(100).currentLoad(20).build(),
+//                new Truck.Builder().velocity(80).weight(5000).fuel("Diesel").milage(1500).milageSinceLastService(1245).name("truck  2").loadCapacity(5000).currentLoad(3000).build(),
+//                new Car.Builder().velocity(80).weight(5000).fuel("Diesel").milage(1500).milageSinceLastService(1245).name("car  1").brand("Fiat").model("x25").build()
                 ));
 
         for ( Vehicle v : vehicleList){
